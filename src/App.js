50dbef2f1e4a59dc5c3d405df9c7c672f9ed8537
BC-macair028 <bc-macair028@BC-macair028s-MacBook-Air.local>
2021-11-23 10:48:12 +0200

    added currency, population and languages

--- src/App.js
+++ src/App.js
@@ -2,23 +2,47 @@ import React, { Component } from "react";
 import axios from "axios";
 import CountryCard from "./components/CountryCard";
 
+
 class App extends Component {
   state = {
     data: [],
   };
 
   componentDidMount() {
-    axios.get("https://restcountries.com/v3.1/all").then((res) => {
+    axios.get("https://restcountries.com/v2/all?fields=name,capital,flags,languages,population,currencies").then((res) => {
       this.setState({ data: res.data });
   });
   }
 
+  // number = require('easy-number-formatter')
+  // newNum = number.formatNumber(1000)
+
   render() {
     return <div className = "countries">
       {this.state.data.map((country) => (
-        <div className = "country" key={country.name.common}>
-        <h2>{country.name.common}</h2> <p>Capital: {country.capital}</p>
-        <img src={country.flags.png}/>
+        <div className = "country" key={country.name}>
+          <header>
+            <h2>{country.name}</h2> 
+            <h3>Capital: {country.capital}</h3>
+          </header>
+            <img src={country.flags.png}/>
+            <div className="cardContent">
+              <p>
+              Language(s): {country.languages.map((lang, i) =>(
+                <span key={i}> {lang.name} </span>
+                ))}
+                </p> 
+                <p>
+                {/* Population: {numeric(country.population).format("0.0a")} */}
+                {/* Population: {country.population.number.formatNumber(1000)} */}
+                Population: {country.population}
+              </p>
+                <p>
+                  Currencies: {country.currencies.map((cur, i) =>(
+                <span key={i}>{" "} {cur.name} {cur.symbol} </span>
+                ))}
+                </p>
+            </div>
         </div>
       ))};
     </div>;
